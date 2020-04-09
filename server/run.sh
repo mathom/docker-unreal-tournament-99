97b84dd8e672121e65d44d219db7a49db3764ed6
Matthew Thompson <chameleonator@gmail.com>
2020-04-08 23:57:00 -0500

    Update run.sh

--- server/run.sh
+++ server/run.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 
-COMMAND=/bin/bash
-
 docker run \
-    -d
-    --privileged \
+    -v $PWD/UnrealTournament.ini:/opt/ut-server/System/UnrealTournament.ini \
+    -p 7000-7081:7000-7081/udp \
     -p 7777-7781:7777-7781/udp \
     -p 27900:27900/udp \
-    -i -t ut-server
+    -i -t $@ ut-server
